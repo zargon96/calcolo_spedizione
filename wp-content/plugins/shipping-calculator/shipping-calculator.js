515bd38bd5ae4da2e202046b0176daf79343a48d
marco <marco.crucitti96@gmail.com>
2024-05-24 12:59:36 +0200

    t

--- wp-content/plugins/shipping-calculator/shipping-calculator.js
+++ wp-content/plugins/shipping-calculator/shipping-calculator.js
@@ -135,6 +135,305 @@ var provinceMap = {
     "SU (Isole)": "Sud Sardegna (Isole)"
 };
 
+// jQuery(document).ready(function($) {
+//     var opzioniAggiuntiveLabels = {
+//         'sponda_idraulica': 'Consegna con sponda idraulica',
+//         'assicurazione': 'Assicurazione',
+//         'consegna_rapida': 'Consegna rapida'
+//     };
+
+//     $('#tipo_pallet_container').on('click', '.pallet-option', function() {
+//         $('.pallet-option').removeClass('selected');
+//         $(this).addClass('selected');
+//         var selectedPallet = $(this).data('pallet');
+//         $('#tipo_pallet').val(selectedPallet);
+//         disableNextButton(); // Disable the "Avanti" button initially
+//         checkCalculateButton(); // Check the state of the calculate button
+//     });
+
+//     var calculateButton = $('#calculateButton');
+//     var nextbutton = $('#nextbutton');
+    
+//     function checkCalculateButton() {
+//         var selectedPallet = $('#tipo_pallet').val();
+//         var selectedSpedizione = $('input[name="tipo_spedizione"]:checked').val();
+//         if (selectedPallet && selectedSpedizione) {
+//             calculateButton.prop('disabled', false);
+//         } else {
+//             calculateButton.prop('disabled', true);
+//         }
+//     }
+
+//     function updateProvinceDisplay() {
+//         $('#partenza option, #destinazione option').each(function() {
+//             var value = $(this).val();
+//             if (provinceMap[value]) {
+//                 $(this).text(provinceMap[value]);
+//             }
+//         });
+//     }
+
+//     // Chiamata iniziale per aggiornare i nomi delle province
+//     updateProvinceDisplay();
+
+//     var partenzaSelect = $('#partenza');
+//     var destinazioneSelect = $('#destinazione');
+//     var tipoPalletSelect = $('#tipo_pallet');
+//     var opzioniAggiuntiveSelect = $('#opzioni_aggiuntive');
+//     var submitButton = $('#submitButton');
+//     var datiPersonaliDiv = $('#datiPersonali');
+//     var backButton = $('.backButton');
+//     var fields = [
+//         'mittente[nome]', 
+//         'mittente[indirizzo]', 
+//         'mittente[citta]', 
+//         'mittente[cap]', 
+//         'mittente[telefono]', 
+//         'mittente[email]',
+//         'destinatario[nome]', 
+//         'destinatario[indirizzo]', 
+//         'destinatario[citta]', 
+//         'destinatario[cap]', 
+//         'destinatario[telefono]', 
+//         'destinatario[email]'
+//     ];
+
+//     // Disable the "Avanti" button initially
+//     nextbutton.prop('disabled', true);
+
+//     // Initialize Select2 with tagging
+//     $('.js-example-tags').select2({
+//         tags: true,
+//     });
+
+//     function updatePalletTypes() {
+//         var tipoSpedizione = $('input[name="tipo_spedizione"]:checked').val();
+//         var destinazione = destinazioneSelect.val();
+//         tipoPalletSelect.empty();
+//         var options = shippingData[destinazione][tipoSpedizione];
+//         $.each(options, function(palletType) {
+//             tipoPalletSelect.append($('<option>', {
+//                 value: palletType,
+//                 text: palletType
+//             }));
+//         });
+//     }
+    
+//     function updateProvince() {
+//         var partenza = partenzaSelect.val();
+//         var destinazione = destinazioneSelect.val();
+//         $('#provincia_mittente').val(partenza);
+//         $('#provincia_destinatario').val(destinazione);
+//     }
+
+//     // Disable the "Avanti" button when any select field is changed
+//     function disableNextButton() {
+//         nextbutton.prop('disabled', true);
+//     }
+
+//     $('input[name="tipo_spedizione"]').change(function() {
+//         updatePalletTypes();
+//         disableNextButton();
+//         checkCalculateButton();
+//     });
+    
+//     destinazioneSelect.change(function() {
+//         updatePalletTypes();
+//         updateProvince();
+//         disableNextButton();
+//         checkCalculateButton();
+//     });
+    
+//     partenzaSelect.change(function() {
+//         updatePalletTypes();
+//         updateProvince();
+//         disableNextButton();
+//         checkCalculateButton();
+//     });
+
+//     tipoPalletSelect.change(disableNextButton);
+//     opzioniAggiuntiveSelect.change(disableNextButton);
+
+//     // Inizializza i tipi di pallet e le province alla prima esecuzione
+//     updatePalletTypes();
+//     updateProvince();
+//     checkCalculateButton();
+
+//     calculateButton.click(function() {
+//         var partenza = partenzaSelect.val();
+//         var destinazione = destinazioneSelect.val();
+//         var tipoSpedizione = $('input[name="tipo_spedizione"]:checked').val();
+//         var tipoPallet = tipoPalletSelect.val();
+//         var opzioniAggiuntive = [];
+//         $('#opzioni_aggiuntive input:checked').each(function() {
+//             opzioniAggiuntive.push($(this).val());
+//         });
+
+//         // Chiamata AJAX per calcolare il costo della spedizione
+//         $.post('/wp-admin/admin-ajax.php', {
+//             action: 'calculate_shipping',
+//             partenza: partenza,
+//             destinazione: destinazione,
+//             tipoSpedizione: tipoSpedizione,
+//             tipoPallet: tipoPallet,
+//             opzioniAggiuntive: opzioniAggiuntive
+//         }, function(response) {
+//             $('#result').text('Il costo di spedizione è: €' + response);
+//             $('#summaryPartenza').text(provinceMap[partenza] || partenza);
+//             $('#summaryDestinazione').text(provinceMap[destinazione] || destinazione);
+//             $('#summaryTipoSpedizione').text(tipoSpedizione);
+//             $('#summaryTipoPallet').text(tipoPallet);
+
+//             var opzioniAggiuntiveReadable = opzioniAggiuntive.map(function(opzione) {
+//                 return opzioniAggiuntiveLabels[opzione] || opzione;
+//             });
+
+//             if (opzioniAggiuntiveReadable.length > 0) {
+//                 $('#summaryOpzioni').text(opzioniAggiuntiveReadable.join(', '));
+//             } else {
+//                 $('#summaryOpzioni').text('Nessuna opzione aggiuntiva aggiunta');
+//             }
+            
+//             $('#summaryCosto').text('€' + response);
+//             $('#summary').removeClass('hidden'); // Mostra il riepilogo
+//             nextbutton.show();
+//             nextbutton.prop('disabled', false); // Enable the "Avanti" button
+//         }).fail(function() {
+//             alert('Errore nel calcolo del costo di spedizione!');
+//         });
+//     });
+
+//     nextbutton.click(function() {
+//         $('#spedizioneForm').hide();
+//         datiPersonaliDiv.show(); // Mostra i campi dei dati anagrafici
+//     });
+
+//     backButton.click(function() { 
+//         datiPersonaliDiv.hide();
+//         $('#spedizioneForm').show(); // Mostra il modulo di spedizione
+//         disableNextButton(); // Disable the "Avanti" button when going back
+//     });
+
+//     submitButton.click(function() {
+//         var form = $('#spedizioneForm')[0];
+//         if (!form.checkValidity()) {
+//             alert('Per favore, compila tutti i campi obbligatori.');
+//             return;
+//         }
+
+//         var nomeMittente = $('#mittente\\[nome\\]').val();
+//         var indirizzoMittente = $('#mittente\\[indirizzo\\]').val();
+//         var cittaMittente = $('#mittente\\[citta\\]').val();
+//         var capMittente = $('#mittente\\[cap\\]').val();
+//         var telefonoMittente = $('#mittente\\[telefono\\]').val();
+//         var emailMittente = $('#mittente\\[email\\]').val();
+
+//         var nomeDestinatario = $('#destinatario\\[nome\\]').val();
+//         var indirizzoDestinatario = $('#destinatario\\[indirizzo\\]').val();
+//         var cittaDestinatario = $('#destinatario\\[citta\\]').val();
+//         var capDestinatario = $('#destinatario\\[cap\\]').val();
+//         var telefonoDestinatario = $('#destinatario\\[telefono\\]').val();
+//         var emailDestinatario = $('#destinatario\\[email\\]').val();
+
+//         var partenza = $('#partenza').val();
+//         var destinazione = $('#destinazione').val();
+//         var tipoSpedizione = $('input[name="tipo_spedizione"]:checked').val();
+//         var tipoPallet = $('#tipo_pallet').val();
+//         var opzioniAggiuntive = [];
+//         $('#opzioni_aggiuntive input:checked').each(function() {
+//             opzioniAggiuntive.push($(this).val());
+//         });
+//         var costoSpedizione = $('#result').text().split('€')[1].trim();
+
+//         // Chiamata AJAX per inviare la richiesta
+//         $.post('/wp-admin/admin-ajax.php', {
+//             action: 'submit_request',
+//             'mittente[nome]': nomeMittente,
+//             'mittente[indirizzo]': indirizzoMittente,
+//             'mittente[citta]': cittaMittente,
+//             'mittente[cap]': capMittente,
+//             'mittente[telefono]': telefonoMittente,
+//             'mittente[email]': emailMittente,
+//             'destinatario[nome]': nomeDestinatario,
+//             'destinatario[indirizzo]': indirizzoDestinatario,
+//             'destinatario[citta]': cittaDestinatario,
+//             'destinatario[cap]': capDestinatario,
+//             'destinatario[telefono]': telefonoDestinatario,
+//             'destinatario[email]': emailDestinatario,
+//             partenza: partenza,
+//             destinazione: destinazione,
+//             tipoSpedizione: tipoSpedizione,
+//             tipoPallet: tipoPallet,
+//             opzioniAggiuntive: opzioniAggiuntive,
+//             costoSpedizione: costoSpedizione
+//         }, function(response) {
+//             if (response.success) {
+//                 // Mostra l'avviso di successo
+//                 $('#alertContainer').html('<div class="alert alert-success alert-dismissible fade show" role="alert">Richiesta inviata con successo.<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
+//             } else {
+//                 $('.alert').remove(); // Rimuovi eventuali avvisi esistenti
+//                 $('#alertContainer').prepend('<div class="alert alert-danger alert-dismissible fade show" role="alert">Errore: ' + response.data.errors.join('') + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');    
+//                 // Scrolla alla posizione dell'alert di errore
+//                 $('html, body').animate({
+//                     scrollTop: $('#alertContainer').offset().top
+//                 }, 'slow');            
+//             }
+//         }).fail(function() {
+//             $('.alert').remove(); // Rimuovi eventuali avvisi esistenti
+//             $('#alertContainer').prepend('<div class="alert alert-danger alert-dismissible fade show" role="alert">Errore di rete. Per favore riprova più tardi.<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');    
+//             // Scrolla alla posizione dell'alert di errore
+//             $('html, body').animate({
+//                 scrollTop: $('#alertContainer').offset().top
+//             }, 'slow');
+//         });
+//     });
+
+//     // Funzione per permettere solo numeri nei campi CAP e massimo 5 cifre
+//     $('#mittente\\[cap\\], #destinatario\\[cap\\]').on('input', function() {
+//         this.value = this.value.replace(/\D/g, '').substring(0, 5);
+//     });
+
+//     // Funzione per permettere solo numeri nel cellulare con massimo 10 cifre
+//     $('#mittente\\[telefono\\], #destinatario\\[telefono\\]').on('input', function() {
+//         this.value = this.value.replace(/\D/g, '').substring(0, 10);
+//     });
+
+//     // Funzione per permettere solo lettere
+//     function allowOnlyLetters(input) {
+//         var value = input.value;
+//         var regex = /[^a-zA-Z\s]/g;
+//         input.value = value.replace(regex, '');
+//     }
+
+//     // Associa l'evento 'input' ai campi 'nominativo' e 'città'
+//     $('#mittente\\[citta\\],#destinatario\\[citta\\]').on('input', function() {
+//         allowOnlyLetters(this);
+//     });
+
+//     // Funzioni di validazione con classi bootstrap
+//     function validateField(field) {
+//         if (field.checkValidity()) {
+//             field.classList.remove('is-invalid');
+//             field.classList.add('is-valid');
+//         } else {
+//             field.classList.remove('is-valid');
+//             field.classList.add('is-invalid');
+//         }
+//     }
+
+//     // Associa l'evento 'input' a tutti i campi nell'array
+//     $(fields.map(field => `#${field.replace(/\[/g, '\\[').replace(/\]/g, '\\]')}`).join(',')).on('input', function() {
+//         validateField(this);
+//     });
+
+//     // Associa l'evento 'click' al pulsante di submit
+//     submitButton.click(function() {
+//         $(fields.map(field => `#${field.replace(/\[/g, '\\[').replace(/\]/g, '\\]')}`).join(',')).each(function() {
+//             validateField(this);
+//         });
+//     });
+// });
+
 jQuery(document).ready(function($) {
     var opzioniAggiuntiveLabels = {
         'sponda_idraulica': 'Consegna con sponda idraulica',
